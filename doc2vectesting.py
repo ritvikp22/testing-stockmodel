e0c7dc14ac36d8248b688030a992778fe7b4b01f
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-08-11 11:13:36 -0700

    Adding model that accounts for stopwords

--- doc2vectesting.py
+++ doc2vectesting.py
@@ -2,24 +2,34 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 import csv
+import sys
+from nltk.corpus import stopwords
+def remove_stopwords(string):
+    new_string = ""
+    for word in string.split():
+        if word not in stopwords.words("english"):
+            new_string += word + " "
+    return new_string[:len(new_string)-1]
+'''
 ## Exapmple document (list of sentences)
 doc = list()
+csv.field_size_limit(sys.maxsize)
 with open("trumpspeeches2.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     for row in csvreader:
-        for i in range(3, len(rows)):
+        for i in range(3, len(row)):
             doc.append(row[i])
-        break
 # Tokenization of each document
 tokenized_doc = []
 for d in doc:
-    tokenized_doc.append(word_tokenize(d.lower()))
-print(tokenized_doc)
+    if d.lower() not in stopwords.words("english"):
+        tokenized_doc.append(word_tokenize(d.lower()))
+print("TOKENIZED")
 tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(tokenized_doc)]
-print(tagged_data)
+print("TAGGED")
 model = Doc2Vec(tagged_data, vector_size=20, window=2, min_count=1, workers=4, epochs = 100)
-model.save("test_doc2vec.model")
-model= Doc2Vec.load("test_doc2vec.model")
-print(model.wv.vocab)
-test_doc = word_tokenize("That is a good device".lower())
-print(model.docvecs.most_similar(positive=[model.infer_vector(test_doc)],topn=5))
+model.save("test_doc2vec2.model")
+print("SAVED")
+'''
+model= Doc2Vec.load("test_doc2vec2.model")
+print(model.n_similarity(remove_stopwords("morning").split(), remove_stopwords("evening").split()))
