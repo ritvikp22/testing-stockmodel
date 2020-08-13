9b1aa9af30483482be81ad0a6e76f2171fe99e2b
Ritvik Pandey <ritvikp22@gmail.com>
2020-08-13 12:39:53 -0700

    working pf-idf

--- pf_idf.py
+++ pf_idf.py
@@ -1,33 +1,126 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
+import csv
+import sys
 
-documentA = 'the man went out for a walk'
-documentB = 'the children sat around the fire'
+#TF-IDF Test
 
-bagOfWordsA = documentA.split(' ')
-bagOfWordsB = documentB.split(' ')
+# documentA = 'the man went out for a walk'
+# documentB = 'the children sat around the fire'
 
-uniqueWords = set(bagOfWordsA).union(set(bagOfWordsB))
+# bagOfWordsA = documentA.split(' ')
+# bagOfWordsB = documentB.split(' ')
 
-numOfWordsA = dict.fromkeys(uniqueWords, 0)
-for word in bagOfWordsA:
+# uniqueWords = set(bagOfWordsA).union(set(bagOfWordsB))
+
+# numOfWordsA = dict.fromkeys(uniqueWords, 0)
+# for word in bagOfWordsA:
+#     numOfWordsA[word] += 1
+# numOfWordsB = dict.fromkeys(uniqueWords, 0)
+# for word in bagOfWordsB:
+#     numOfWordsB[word] += 1
+
+# stopwords.words('english')
+
+# def computeTF(wordDict, bagOfWords):
+#     tfDict = {}
+#     bagOfWordsCount = len(bagOfWords)
+#     for word, count in wordDict.items():
+#         tfDict[word] = count / float(bagOfWordsCount)
+#     return tfDict
+
+# tfA = computeTF(numOfWordsA, bagOfWordsA)
+# tfB = computeTF(numOfWordsB, bagOfWordsB)
+
+# def computeIDF(documents):
+#     import math
+#     N = len(documents)
+    
+#     idfDict = dict.fromkeys(documents[0].keys(), 0)
+#     for document in documents:
+#         for word, val in document.items():
+#             if val > 0:
+#                 idfDict[word] += 1
+    
+#     for word, val in idfDict.items():
+#         idfDict[word] = math.log(N / float(val))
+#     return idfDict
+
+# idfs = computeIDF([numOfWordsA, numOfWordsB])
+
+# def computeTFIDF(tfBagOfWords, idfs):
+#     tfidf = {}
+#     for word, val in tfBagOfWords.items():
+#         tfidf[word] = val * idfs[word]
+#     return tfidf
+
+# tfidfA = computeTFIDF(tfA, idfs)
+# tfidfB = computeTFIDF(tfB, idfs)
+# df = pd.DataFrame([tfidfA, tfidfB])
+
+# vectorizer = TfidfVectorizer()
+# vectors = vectorizer.fit_transform([documentA, documentB])
+# feature_names = vectorizer.get_feature_names()
+# dense = vectors.todense()
+# denselist = dense.tolist()
+# df = pd.DataFrame(denselist, columns=feature_names)
+# df.to_csv('pd_idf_test.csv')
+
+
+#PF-IDF Test
+
+docs = dict()
+csv.field_size_limit(sys.maxsize)
+
+count = 0
+transcripts = dict()
+with open("trumpspeeches2.csv", 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        transcripts[count] = row[1]
+        docs[count] = []
+        for i in range(3, len(row)):
+            docs[count].append(row[i])
+        count += 1
+
+
+
+for i in range(len(docs)):
+    for element in docs[i]:
+        element = element.lower()
+        string = 'Inaudible'
+        if string in element:
+            element = element.replace(string, '')
+
+docA = docs[0]
+docB = docs[1]
+
+totalA = transcripts[0]
+totalB = transcripts[1]
+
+unique_phrases = set(docA).union(set(docB))
+# print(unique_phrases)
+
+numOfWordsA = dict.fromkeys(unique_phrases, 0)
+for word in docA:
     numOfWordsA[word] += 1
-numOfWordsB = dict.fromkeys(uniqueWords, 0)
-for word in bagOfWordsB:
+numOfWordsB = dict.fromkeys(unique_phrases, 0)
+for word in docB:
     numOfWordsB[word] += 1
 
-stopwords.words('english')
 
-def computeTF(wordDict, bagOfWords):
+def computeTF(phraseDict, bag_phrase):
     tfDict = {}
-    bagOfWordsCount = len(bagOfWords)
-    for word, count in wordDict.items():
-        tfDict[word] = count / float(bagOfWordsCount)
+    bag_phrase_count = len(bag_phrase)
+    for phrase, count in phraseDict.items():
+        tfDict[phrase] = count / float(bag_phrase_count)
     return tfDict
 
-tfA = computeTF(numOfWordsA, bagOfWordsA)
-tfB = computeTF(numOfWordsB, bagOfWordsB)
+tfA = computeTF(numOfWordsA, docA)
+tfB = computeTF(numOfWordsB, docB)
+
+print(tfA, tfB)
 
 def computeIDF(documents):
     import math
@@ -35,30 +128,25 @@ def computeIDF(documents):
     
     idfDict = dict.fromkeys(documents[0].keys(), 0)
     for document in documents:
-        for word, val in document.items():
+        for phrase, val in document.items():
             if val > 0:
-                idfDict[word] += 1
+                idfDict[phrase] += 1
     
-    for word, val in idfDict.items():
-        idfDict[word] = math.log(N / float(val))
+    for phrase, val in idfDict.items():
+        idfDict[phrase] = math.log(N / float(val))
     return idfDict
 
 idfs = computeIDF([numOfWordsA, numOfWordsB])
 
+
 def computeTFIDF(tfBagOfWords, idfs):
     tfidf = {}
-    for word, val in tfBagOfWords.items():
-        tfidf[word] = val * idfs[word]
+    for phrase, val in tfBagOfWords.items():
+        tfidf[phrase] = val * idfs[phrase]
     return tfidf
 
 tfidfA = computeTFIDF(tfA, idfs)
 tfidfB = computeTFIDF(tfB, idfs)
 df = pd.DataFrame([tfidfA, tfidfB])
 
-vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform([documentA, documentB])
-feature_names = vectorizer.get_feature_names()
-dense = vectors.todense()
-denselist = dense.tolist()
-df = pd.DataFrame(denselist, columns=feature_names)
 df.to_csv('pd_idf_test.csv')
